9b48f0cde2b241caf88ec29c5887ffd5cad7f3a0
V1leDev <alexander02plaikner.ap@gmail.com>
2020-11-28 15:46:40 +0100

    Refactor Timer files location

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <net-snmp/net-snmp-includes.h>
 #include <vector>
 #include <string>
-#include "Timer.h"
+#include "Benchmarking/Timer.h"
 
 #define INPUT_IP 1
 #define DEVELOPMENT_INFORMATION 1
