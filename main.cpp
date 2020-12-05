bca94cd32d3cfa6da1c112b28fc127bfdab27535
V1leDev <alexander02plaikner.ap@gmail.com>
2020-12-05 11:34:05 +0100

    Add initial Qt files

--- main.cpp
+++ main.cpp
@@ -7,22 +7,28 @@
 #include "SNMP/Scanner.h"
 #include "SNMP/Session.h"
 #include "Utility/IPRangeCalculator.h"
+#include <QApplication>
+#include "UI/mainwindow.h"
 
 
+#define DEVELOPMENT_INFORMATION 0
+#define SCAN 0
 
-#define DEVELOPMENT_INFORMATION 1
-
-int main() {
+int main(int argc, char *argv[]) {
 #if DEVELOPMENT_INFORMATION == 1
     std::cout << "WORK IN PROGRESS - DOES NOT REPRESENT THE FINAL PRODUCT" << std::endl;
     std::cout << "Functionality: Get predefined set of information from client running an SNMP agent" << std::endl;
     std::cout << "Tested with SN-NAS\n" << std::endl;
 #endif
+#if SCAN == 1
     {
         Timer t;
         Scanner s;
         s.scan();
     }
-
-    return 0;
+#endif
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
