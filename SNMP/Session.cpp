0b0a78ff5a17d0b31d6ddb3994754387f7ec1a28
V1leDev <alexander02plaikner.ap@gmail.com>
2020-12-16 07:41:19 +0100

    Cleanup code

--- SNMP/Session.cpp
+++ SNMP/Session.cpp
@@ -27,16 +27,12 @@ void Session::start_session(QListWidget *results_list, u_char *community) {
 
     // set SNMPv1 community name used for authentication
     session.community = community;
-    //std::cout << community << std::endl;
+
     session.community_len = strlen(reinterpret_cast<const char *>(session.community));
-    //session.timeout = 200000;
-    //snmp_set_do_debugging(1);
+
     // Establish the session
     ss = snmp_sess_open(&session);
 
-    //1000000 vs 100000
-    //ss->timeout = 200000;
-    //ss->retries = 3;
 
     if (!ss) {
         snmp_sess_perror("Session not created", &session);
@@ -44,33 +40,24 @@ void Session::start_session(QListWidget *results_list, u_char *community) {
     }
     for (int i = 0; i < request_strings.size(); ++i) {
         // Create the PDU for the data for our request
-        // We're going to GET the system.sysDescr.0 node
         pdu = snmp_pdu_create(SNMP_MSG_GETNEXT);
         anOID_len = MAX_OID_LEN;
 
         if (!snmp_parse_oid(request_strings.at(i), anOID, &anOID_len)) {
             snmp_perror(request_strings.at(i));
-            //exit(1);
         }
 
         snmp_add_null_var(pdu, anOID, anOID_len);
 
-        // mlock.lock();
         // Send the request out
-        //status = snmp_synch_response(ss, pdu, &response);
         status = snmp_sess_synch_response(ss, pdu, &response);
-        // mlock.unlock();
+
         // Process the response
         if (status == STAT_SUCCESS && nullptr != response) {
-            // SUCCESS: Print the result variables
-
+            // SUCCESS: Continue with writing response to UI
             for (vars = response->variables; vars; vars = vars->next_variable) {
-                // Mutex? If data is later on stored in vector anyway then Mutex is a must
-                //std::cout << ip << ":";
-                //print_variable(vars->name, vars->name_length, vars);
                 char buf[1024];
                 snprint_variable(buf, sizeof(buf), vars->name, vars->name_length, vars);
-                //fprintf(stdout, "%s: %s\n", ip.c_str(), buf);
                 std::string message = ip.c_str();
                 message.append(":");
                 message.append(buf);
--- UI/mainwindow.cpp
+++ UI/mainwindow.cpp
@@ -24,88 +24,83 @@ MainWindow::~MainWindow() {
 }
 
 
-void MainWindow::on_radioButton_whitemode_clicked()
-{
+void MainWindow::on_radioButton_whitemode_clicked() {
     this->setStyleSheet("background-color: white; ");
 }
 
-void MainWindow::on_radioButton_darkmode_clicked()
-{
+void MainWindow::on_radioButton_darkmode_clicked() {
     this->setStyleSheet("background-color: #97949a; color: white");
 }
 
 
-void MainWindow::on_radioButton_rebeccapurple_clicked()
-{
+void MainWindow::on_radioButton_rebeccapurple_clicked() {
     this->setStyleSheet("background-color: #663399; color: white");
 }
 
-void MainWindow::on_checkBox_scan_network_range_clicked()
-{
+void MainWindow::on_checkBox_scan_network_range_clicked() {
     ui->label_subnet_slash->setEnabled(ui->checkBox_scan_network_range->isChecked());
     ui->lineEdit_subnet_input->setEnabled(ui->checkBox_scan_network_range->isChecked());
-    if (!ui->checkBox_scan_network_range->isChecked()){
+    if (!ui->checkBox_scan_network_range->isChecked()) {
         ui->lineEdit_subnet_input->clear();
     }
 }
 
-void MainWindow::on_buttonBox_oid_input_accepted()
-{
-    if(ui->lineEdit_oid_input->text()!=""){
+void MainWindow::on_buttonBox_oid_input_accepted() {
+    if (ui->lineEdit_oid_input->text() != "") {
         ui->listWidget_oids->addItem(ui->lineEdit_oid_input->text());
         ui->lineEdit_oid_input->clear();
     }
 }
 
-void MainWindow::on_buttonBox_oid_input_rejected()
-{
+void MainWindow::on_buttonBox_oid_input_rejected() {
     ui->lineEdit_oid_input->clear();
 }
 
-void MainWindow::on_pushButton_remove_oid_clicked()
-{
+void MainWindow::on_pushButton_remove_oid_clicked() {
     qDeleteAll(ui->listWidget_oids->selectedItems());
 }
 
-void MainWindow::on_lineEdit_oid_input_returnPressed()
-{
-    if(ui->lineEdit_oid_input->text()!=""){
+void MainWindow::on_lineEdit_oid_input_returnPressed() {
+    if (ui->lineEdit_oid_input->text() != "") {
         ui->listWidget_oids->addItem(ui->lineEdit_oid_input->text());
         ui->lineEdit_oid_input->clear();
     }
 
 }
 
-void MainWindow::on_pushButton_scan_clicked()
-{
+void MainWindow::on_pushButton_scan_clicked() {
     ui->listWidget_results->clear();
-    if(ui->checkBox_scan_network_range->isChecked()){
-        s->setHosts(IPRangeCalculator::calculate_ips(ui->lineEdit_ip_1->text().toInt(), ui->lineEdit_ip_2->text().toInt(), ui->lineEdit_ip_3->text().toInt(), ui->lineEdit_ip_4->text().toInt(), ui->lineEdit_subnet_input->text().toInt()));
+    if (ui->checkBox_scan_network_range->isChecked()) {
+        s->setHosts(
+                IPRangeCalculator::calculate_ips(ui->lineEdit_ip_1->text().toInt(), ui->lineEdit_ip_2->text().toInt(),
+                                                 ui->lineEdit_ip_3->text().toInt(), ui->lineEdit_ip_4->text().toInt(),
+                                                 ui->lineEdit_subnet_input->text().toInt()));
         s->removeFirstLastHost();
-    }else{
-        s->setHosts(IPRangeCalculator::calculate_ips(ui->lineEdit_ip_1->text().toInt(), ui->lineEdit_ip_2->text().toInt(), ui->lineEdit_ip_3->text().toInt(), ui->lineEdit_ip_4->text().toInt(), 32));
+    } else {
+        s->setHosts(
+                IPRangeCalculator::calculate_ips(ui->lineEdit_ip_1->text().toInt(), ui->lineEdit_ip_2->text().toInt(),
+                                                 ui->lineEdit_ip_3->text().toInt(), ui->lineEdit_ip_4->text().toInt(),
+                                                 32));
     }
-    for (int row = 0; row < ui->listWidget_oids->count(); ++row)
-    {
+    for (int row = 0; row < ui->listWidget_oids->count(); ++row) {
         oids.emplace_back(strdup(ui->listWidget_oids->item(row)->text().toStdString().c_str()));
     }
     s->setOids(oids);
-    for(auto item : this->oids){
+    for (auto item : this->oids) {
         free((void *) item);
     }
     oids.clear();
-    if(ui->checkBox_community->isChecked() && ui->lineEdit_community->text()!= ""){
-        community = (u_char *)ui->lineEdit_community->text().toStdString().c_str();
-    }else{
-        community = (u_char *)"public";
+    if (ui->checkBox_community->isChecked() && ui->lineEdit_community->text() != "") {
+        community = (u_char *) ui->lineEdit_community->text().toStdString().c_str();
+    } else {
+        community = (u_char *) "public";
     }
     s->scan(ui->listWidget_results, community);
 }
 
-void MainWindow::on_checkBox_community_clicked()
-{
+void MainWindow::on_checkBox_community_clicked() {
     ui->lineEdit_community->setEnabled(ui->checkBox_community->isChecked());
-    if(!ui->checkBox_community->isChecked()){
+    if (!ui->checkBox_community->isChecked()) {
         ui->lineEdit_community->clear();
     }
 }
