979e29d07c5f1735445630ced0a61d50cd278759
V1leDev <alexander02plaikner.ap@gmail.com>
2021-01-06 11:08:24 +0100

    Update comments in Scanner.cpp

--- SNMP/Scanner.cpp
+++ SNMP/Scanner.cpp
@@ -7,73 +7,73 @@
 
 
 Scanner::Scanner() {
-    // initialize SNMP library
+    // initializes SNMP library
     init_snmp("snmpapp");
 }
 
-// set up threads and sessions so that an IP or subnet can be scanned
+// sets up threads and sessions so that an IP or subnet can be scanned
 void Scanner::scan(QListWidget *results_list, u_char *community) {
-    // vector to store each thread that works on a session
+    // stores each thread that works on a session
     std::vector<std::thread> threads;
-    // vector to store all sessions
+    // stores all sessions
     std::vector<Session> sessions;
     for (const auto &host : hosts) {
-        // store sessions in vector
+        // creates sessions in vector
         sessions.emplace_back(Session(host, oids));
     }
-    // reserve space in threads vector --> less resizing(copying)
+    // reserves space in threads vector --> less resizing(copying)
     threads.reserve(sessions.size());
     for (const auto &session : sessions) {
-        // start every thread and give it a session to work on
+        // starts every thread and gives it a session to work on
         threads.emplace_back(std::thread(&Session::start_session, session, results_list, community));
     }
     for (auto &t : threads) {
-        // detach each thread so it can work independently
+        // detaches each thread so it can work independently
         t.detach();
     }
 }
 
-// set hosts that are to be scanned later on
+// sets hosts that are to be scanned later on
 void Scanner::setHosts(const std::vector<std::string> &hosts) {
     Scanner::hosts = hosts;
 }
 
-// get hosts
+// gets hosts
 const std::vector<std::string> &Scanner::getHosts() const {
     return hosts;
 }
 
-// remove netid and broadcast ip from hosts because they don't need to be scanned
+// removes netid and broadcast IP from hosts because they don't need to be scanned
 void Scanner::removeFirstLastHost() {
     hosts.erase(hosts.begin());
     hosts.erase(hosts.end());
 }
 
-// set OIDs that are requested later on when scanning
+// sets OIDs that are requested later on when scanning
 void Scanner::setOids(const std::vector<const char *> &oids) {
-    // free each oid in oids vector (has to be done because they are dynamically allocated using strdup())
+    // frees each oid in oids vector (has to be done because they are dynamically allocated using strdup())
     for (auto item : this->oids) {
         free((void *) item);
     }
-    // clear pointers in oids vector
+    // clears pointers in oids vector
     this->oids.clear();
 
     for (auto oid : oids) {
-        // duplicate OIDs into vector (dynamically allocated)
+        // duplicates OIDs into vector (dynamically allocated)
         this->oids.push_back(strdup(oid));
     }
 }
 
-// get OIDs
+// gets OIDs
 const std::vector<const char *> &Scanner::getOids() const {
     return oids;
 }
 
 Scanner::~Scanner() {
     for (auto item : oids) {
-        // free each oid in oids vector (has to be done because they are dynamically allocated using strdup())
+        // frees each oid in oids vector (has to be done because they are dynamically allocated using strdup())
         free((void *) item);
     }
-    // clear pointers in oids vector
+    // clears pointers in oids vector
     oids.clear();
 }
